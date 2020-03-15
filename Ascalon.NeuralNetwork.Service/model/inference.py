ccd1d3e9de47104abaf6bc0e641f61b379b027f4
Mirko Anton <antonmirko.ukr@gmail.com>
2020-03-16 01:55:29 +0300

    Добавил в проект кафку

--- Ascalon.NeuralNetwork.Service/model/inference.py
+++ Ascalon.NeuralNetwork.Service/model/inference.py
@@ -2,7 +2,25 @@ import numpy as np
 
 from network import create_model
 
-from flask import Flask, jsonify, request
+from threading import Thread
+
+from json import loads, dumps
+
+from kafka import KafkaConsumer, KafkaProducer
+
+from flask import Flask
+
+
+class Result:
+
+    def __init__(self, result):
+        self.__x = result
+
+    def result(self):
+        return self.__x
+
+    def set_result(self, result):
+        self.__x = result
 
 app = Flask(__name__)
 
@@ -12,27 +30,47 @@ net = create_model(training=False)
 
 net.load_weights(f'../ckpt/{filename}')
 
+producer = KafkaProducer(bootstrap_servers=['localhost:29092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+consumer = KafkaConsumer(
+            'neuralnetwork_data',
+            bootstrap_servers=['localhost:29092'],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id='NeuralNetworkService',
+            value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 def predict(x):
     pred = net.predict(x)
     label = np.argmax(pred, axis=1)  # [0]
     return label
 
-@app.route('/predict', methods=['POST'])
-def create_task():
-    if request.data:
-        tasks = []
-        for j in request.get_json():
-            task = [float(j['Gfx']), float(j['Gfy']), float(j['Gfz']),
-                    float(j['Wx']), float(j['Wy']), float(j['Speed']),
-                    float(j['Wz'])]
-            tasks.append(task)
-        result = np.reshape(tasks, (-1, 50, 7))
-        for element in result:
-            element[:] = element - element.mean(axis=0)
-        something = predict(result)
-    return jsonify(result=str(something[0]+1)), 200
 
+def KafkaConsumer():
+    while True:
+        tasks = []
+        label = 0
+        for message in consumer:
+            for data in message.value:
+                task = [float(data['Gfx']), float(data['Gfy']), float(data['Gfz']),
+                        float(data['Wx']), float(data['Wy']), float(data['Speed']),
+                        float(data['Wz'])]
+                label = float(data['Label'])
+                tasks.append(task)
+            result = np.reshape(tasks, (-1, 50, 7))
+            for element in result:
+                element[:] = element - element.mean(axis=0)
+            something = predict(result)
+            result = Result(something[0])
+            print('{} = {}'.format(something[0], label))
+            data = {'result': int(something[0]), 'label': label}
+            producer.send('client_service_data', value=data)
+
+
+kafkaConsumer = Thread(target=KafkaConsumer)
+kafkaConsumer.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
